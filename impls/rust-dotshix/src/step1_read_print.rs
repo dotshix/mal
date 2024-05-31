54962722323113c4952b2793afe3a8469b1bf4e3
Dario <dariosans@gmail.com>
2024-05-31 15:17:26 -0400

    Moved print functions to src/printer.rs

--- impls/rust-dotshix/src/step1_read_print.rs
+++ impls/rust-dotshix/src/step1_read_print.rs
@@ -1,4 +1,5 @@
 mod reader;
+mod printer;
 
 use pest::error::Error;
 use rustyline::config::Configurer;
@@ -6,6 +7,7 @@ use rustyline::error::ReadlineError;
 use rustyline::{DefaultEditor, Result};
 use std::result::Result as StdResult;
 use reader::mal_parser::{MalValue, Rule, parse_input, format_pest_error};
+use printer::mal_printer::print_node;
 
 fn read(input: String) -> StdResult<Vec<MalValue>, Error<Rule>> {
     parse_input(&input)
@@ -14,50 +16,6 @@ fn read(input: String) -> StdResult<Vec<MalValue>, Error<Rule>> {
 fn eval(input: Vec<MalValue>) -> Vec<MalValue> {
     // For now, eval just returns the input
     input
-}
-
-fn print_list(list: &Vec<MalValue>, open_delim: &str, close_delim: &str) {
-    print!("{}", open_delim);
-    let mut firsttime = true;
-    for v in list {
-        if !firsttime {
-            print!(" ");
-        }
-        print_node(v);
-        firsttime = false;
-    }
-    print!("{}", close_delim);
-}
-
-fn print_node(node: &MalValue) {
-    match node {
-        MalValue::String(s) => print!("{}", s),
-        MalValue::Symbol(s) => print!("{}", s),
-        MalValue::Number(n) => print!("{}", n),
-        MalValue::Bool(b) => print!("{}", b),
-        MalValue::Nil => print!("nil"),
-        MalValue::Round(r) => {
-            print_list(r, "(", ")");
-        }
-        MalValue::Square(r) => {
-            print_list(r, "[", "]");
-        }
-        MalValue::Curly(r) => {
-            print_list(r, "{", "}");
-        }
-
-        MalValue::Comment(c) => print!("{}", c),
-        MalValue::NonSpecialSeq(s) => print!("{}", s),
-        MalValue::Mal(content) => {
-            for (i, item) in content.iter().enumerate() {
-                if i > 0 {
-                    print!(" ");
-                }
-                print_node(item);
-            }
-        }
-        MalValue::EOI => {} // Do nothing for EOI
-    }
 }
 
 fn print(input: Vec<MalValue>) -> String {
